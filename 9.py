c199ec90f23a17cb8f58f0f5a4139ff6f8b22303
Valentín Schiaffino <valenschg@gmail.com>
2021-04-09 10:56:55 -0300

    day 9 done

--- 9.py
+++ 9.py
@@ -1,7 +1,28 @@
 def main():
     numbers = readNumbers()   
-    print(getEncodingError(numbers))
-    
+    encodingError = getEncodingError(numbers)
+    print(encodingError)
+    weakness = findWeakness(numbers, encodingError)
+    print(weakness)
+
+def findWeakness(numbers, encodingError):
+    for i, number in enumerate(numbers):
+        sum = 0
+        j = i
+        while 1:
+            if  j >= len(numbers):
+                break
+            sum += numbers[j]
+            if(sum == encodingError):
+                sumNumbers = numbers[i:j + 1]
+                sumNumbers.sort()
+                _max = sumNumbers[-1]
+                _min = sumNumbers[0]
+                return _max + _min
+            elif sum > encodingError:
+                break
+            j += 1
+
 def getEncodingError(numbers, preambleLength = 25):
     lastNumbers = []
     for number in numbers:
